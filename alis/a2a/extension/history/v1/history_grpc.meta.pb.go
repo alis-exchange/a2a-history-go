dbdd6a6c86c4ac3938c7c510c72e3cafbd35058f
Alis Build <admin@alis.build>
2026-03-05 08:54:13 +0000

    Generate code for version 1.0.405

--- alis/a2a/extension/history/v1/history_grpc.meta.pb.go
+++ alis/a2a/extension/history/v1/history_grpc.meta.pb.go
@@ -3,7 +3,7 @@
 // Source: alis/a2a/extension/history/v1/history.proto
 // Plugin version: v0.0.1
 //
-// Generated on: 2026-03-04 12:55:18 UTC
+// Generated on: 2026-03-05 08:54:13 UTC
 
 package v1
 
