11c8ab46a0a444a687b4fd84fb927232f389b178
Alis Build <admin@alis.build>
2026-03-10 12:01:59 +0000

    Generate code for version 1.0.433

--- alis/a2a/extension/history/v1/history_grpc.meta.pb.go
+++ alis/a2a/extension/history/v1/history_grpc.meta.pb.go
@@ -3,7 +3,7 @@
 // Source: alis/a2a/extension/history/v1/history.proto
 // Plugin version: v0.0.1
 //
-// Generated on: 2026-03-10 11:47:23 UTC
+// Generated on: 2026-03-10 12:01:58 UTC
 
 package v1
 
